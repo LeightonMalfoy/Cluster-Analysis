37bee6271d42f661f7344cf712505c4c4be61a1e
Litong Huang <leightonh@pku.edu.cn>
2019-09-06 16:31:57 -0700

    Add files via upload

--- 7_plot_RL_distr.py
+++ 7_plot_RL_distr.py
@@ -37,9 +37,9 @@ dPar  = {   'aMc'         :  np.array( [2.5,2.6,3.0,3.5,4.0]),#np.array([3.0, 4.
 #                            load data, select events
 #================================================================================
 curr_Mc = dPar['aMc'][0]
-klist = [30,30,30]#150
+klist = [20,20,20,20,20]#150
 minklst = [50]#80, ,
-namelist=['Baja']#,'Northridge','HectorMine','Landers'
+namelist=['Baja','Northridge','HectorMine','Landers','JoshuaTree']#
 
 for name,k in zip(namelist,klist):
     # load nearest neighbor distances
@@ -62,8 +62,11 @@ for name,k in zip(namelist,klist):
     ## l
     #l = 10 ** (-3.22 + 0.69 * eqCat.data['Mag'][0]) / 2 # unit: given by wells and coppersman
     #l=10**(0.44*eqCat.data['Mag'][0]-2) # given by AGU paper
-    L = 1.99*10 ** (0.49 * eqCat.data['Mag'][0]-3)# calc by myself according to AGU
+    #L = 1.99*10 ** (0.49 * eqCat.data['Mag'][0]-3)# calc by myself according to AGU
     ## Rl
+    f_M0=eqCat.data['Mag'][0]
+    f_deltaTau = 3*1e6# stress drop
+    L = ( 4*f_M0/(np.pi*f_deltaTau))**(1./3)*1e-3/2
     aRl = HD / L
     #======================================
     # histogram
@@ -143,62 +146,9 @@ for name,k in zip(namelist,klist):
     # =================================7==============================================
     #                          plot histogram
     # ================================================================================
-    #myplot.plot_rate_pd(aBins, aHist, curr_Mc, 'h_RL1_%s' % name, dPar['eta_binsize'],aRate_bin=aRate_bin,aRate=aRate)
+    myplot.plot_rate_pd(aBins, aHist, curr_Mc, 'RL_distr_%s' % name, dPar['eta_binsize'],aRate_bin=aRate_bin,aRate=aRate)
     #myplot.plot_rate_pd(aBins_NND, aHist_NND, curr_Mc, 'h_NND_%s' % name, dPar['nnd_binsize'])
     #myplot.plot_loglog(aBins, aHist, aRate_bin, aRate, curr_Mc, 'h_lgRL1_%s' % name, dPar['eta_binsize'])
     #myplot.plot_rate_pd(aBins_HD, aHist_HD, curr_Mc, name, dPar['hd_binsize'],Mag = Mag,YR=YR,L=L,plotkind='HD',aRate_bin=aRateBin_HD,aRate=aRate_HD)
-    myplot.plot_loglog(aBins_HD, aHist_HD, curr_Mc, name, Mag = Mag, YR=YR, L=L, plotkind='lgHD4', aRate_bin=aRateBin_HD, aRate=aRate_HD,q=0.35,d=1.2,gamma=0.6,n=-1.35,c=1)#
+    #myplot.plot_loglog(aBins_HD, aHist_HD, curr_Mc, name, Mag = Mag, YR=YR, L=L, plotkind='lgHD4', aRate_bin=aRateBin_HD, aRate=aRate_HD,q=0.35,d=1.2,gamma=0.6,n=-1.35,c=1)#
     #myplot.plot_cumlog(aBins_HD, aHist_HD, aCum_HD, curr_Mc, name, Mag = Mag, YR=YR,  plotkind='HDcum')#
-
-'''
-for curr_Mc,k in zip(dPar['aMc'],dPar['k']):
-    # cut below current completeness
-    eqCatMc.copy(eqCat)
-    eqCatMc.selectEvents(curr_Mc, None, 'Mag')
-
-
-    # load nearest neighbor distances
-    NND_file = 'data/%s_NND_Mc_%.1f_HD.mat'%( file_in.split('.')[0], curr_Mc)
-    dNND   = data_utils.loadmat(NND_file) #,  struct_as_record=True)
-    # ==================================2=============================================
-    #                       compute space-time-magnitude distance, histogram
-    # ================================================================================
-    # only plot for clusterred events
-    dCluster = {}
-    eqCatCluster = EqCat()
-    eqCatCluster.data['Mag'] = np.array([])
-    dNND['aNND'] = np.log10(dNND['aNND'])
-    catChild.copy(eqCatMc)
-    catParent.copy(eqCatMc)
-    # catChild, catPar = create_parent_child_cat( projCat, dNND)
-    catChild.selEventsFromID(dNND['aEqID_c'], repeats=True)
-    catParent.selEventsFromID(dNND['aEqID_p'], repeats=True)
-    print('size of parent catalog', catParent.size(), 'size of offspring cat', catChild.size())
-    # HD
-    HD = np.array([])
-    for iEv in list(range(catParent.size())):
-        if dNND['aNND'][iEv] < dPar['eta_0']:  # cluster events
-            eqCatCluster.data['Mag']=np.append(eqCatCluster.data['Mag'],catParent.data['Mag'][iEv])
-            HD=np.append(HD, dNND['aHD'][iEv])
-    print('current catalog size: ', len(HD))
-    #print('*************************', HD)
-    ## l
-    sigma = 5e6
-    Mw = eqCatCluster.data['Mag']
-    M0 = 10 ** (1.5 * (Mw + 6.03))
-    l = 0.001*(7 / 16 * M0 / sigma) ** (1 / 3)  # meter -> kilomiter
-    ## Rl
-    aRl = HD / l
-    ###histogram
-    aBins = np.arange(0, 20, dPar['eta_binsize'])
-    aHist, aBins = np.histogram(aRl, aBins)
-    aHist, aBins = np.array(list(zip(*zip(aHist, aBins))))  # cut to same length
-    # correct for binsize
-    aHist /= dPar['eta_binsize']
-    aRate_bin, aRate = seis_utils.eqRate(aRl,k)
-    # =================================4==============================================
-    #                          plot histogram
-    # ================================================================================
-    #myplot.plot_rate_pd(aBins, aHist, aRate_bin, aRate, curr_Mc, 'c_cl', dPar['eta_binsize'])
-    myplot.plot_loglog(aBins, aHist, aRate_bin, aRate, curr_Mc, 'd_cl', dPar['eta_binsize'])
-'''
